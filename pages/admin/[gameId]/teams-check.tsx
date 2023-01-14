94d775bbcf58e1e4d1a11abbdcf2c76d79bc6f06
Ilya <elijahmg25@gmail.com>
2023-01-14 11:07:13 +0100

    feat: dynamic data

--- pages/admin/[gameId]/teams-check.tsx
+++ pages/admin/[gameId]/teams-check.tsx
@@ -1,13 +1,19 @@
 import { Button, Stack, Heading } from '@chakra-ui/react';
 
 export default function TeamsCheck() {
+  const teams = [
+    { id: 1, name: 'Team 1' },
+    { id: 2, name: 'Team 2' },
+    { id: 3, name: 'Team 3' },
+  ];
+
   return (
     <Stack>
       <Stack>
         <Heading as="h2">Teams overview</Heading>
-        <Button>Team 1</Button>
-        <Button>Team 2</Button>
-        <Button>Team 3</Button>
+        {teams.map(({ name, id }) => (
+          <Button key={id}>{name}</Button>
+        ))}
       </Stack>
       <Button>Check answers</Button>
     </Stack>
