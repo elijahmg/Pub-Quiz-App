bb61e7a284c6b7e96fdf189549cdfd29cea08d6c
Ilya <elijahmg25@gmail.com>
2023-01-14 17:17:28 +0100

    feat: example

--- pages/[gameId]/index.tsx
+++ pages/[gameId]/index.tsx
@@ -11,6 +11,7 @@ import { useEffect } from 'react';
 import Header from '../../components/headers/Header';
 import SubHeader from '../../components/headers/SubHeader';
 import SubTitle from '../../components/headers/SubTitle';
+import DummyPeople from '../../components/images/dummy-people';
 
 const Welcome = (): JSX.Element => {
   const [value, setValue] = React.useState<string>(``);
@@ -36,6 +37,7 @@ const Welcome = (): JSX.Element => {
           You can name your team however you want. important thing is to have
           fun
         </SubTitle>
+        <DummyPeople />
         <FormControl id="first-name" isInvalid={buttonStatus}>
           <Heading as="h3" mb={4}>
             Team name
