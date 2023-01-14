3f1e91a8a5603726e1757dc280693b7c974ac0a4
Evgeny Pastushek <evgeny.pastushek@sinnerschrader.com>
2023-01-14 16:48:18 +0100

    cr fixes

--- components/headers/quiz-head.tsx
+++ components/headers/quiz-head.tsx
@@ -1,36 +1,46 @@
 import { Badge, Box, Flex, Input, Spacer, Text } from '@chakra-ui/react';
 
-function Topic(props: { topicName: string }) {
+interface QuizHeadProps {
+  teamName: string;
+  round: string;
+  topicName: string;
+}
+
+interface TopicProps {
+  topicName: string;
+}
+
+function Topic({ topicName }: TopicProps) {
   return (
     <Box pt={4} pb={4}>
       <Input
         backgroundColor={'#edf2f7'}
         style={{ opacity: 1 }}
-        value={props.topicName}
+        value={topicName}
         disabled
       />
     </Box>
   );
 }
 
-export default function QuizHead(props: {
-  teamName: string;
-  round: string;
-  topicName: string;
-}) {
+export default function QuizHead({
+  teamName,
+  round,
+  topicName
+}: QuizHeadProps) {
   return (
     <Box>
       <Flex>
         <Box pt={4} pb={4}>
-          <Text as="b">{props.teamName}</Text>
+          <Text as="b">{teamName}</Text>
         </Box>
         <Spacer />
         <Box pt={4} pb={4}>
-          <Badge>Round {props.round}</Badge>
+          <Badge>Round {round}</Badge>
         </Box>
       </Flex>
 
-      <Topic topicName={props.topicName} />
+      <Topic topicName={topicName} />
     </Box>
   );
 }
