eb3ccf034d06409bfe0b892927893d421b60344d
Aleksandr Sedymov <aliaksandr.siadymau@accenture.com>
2023-01-14 14:18:18 +0100

    feat: implement create new quiz with modal window

--- pages/admin/edit/create-new-quiz.tsx
+++ pages/admin/edit/create-new-quiz.tsx
@@ -1,54 +1,16 @@
 import TeamName from '../../../components/TeamName';
-import { Heading, VStack, Button, Input, Box, Text } from '@chakra-ui/react';
-
-interface QuizProps {
-  quizName: string;
-  password: string;
-  usePin: number;
-  numberOfRounds: number;
-  topics: string[];
-  questions: string[];
-}
+import { Button } from '@chakra-ui/react';
+import { NewGameModal } from '../../../components/NewGameModal';
+import { useState } from 'react';
 
 export default function CreateNewQuiz(name: string) {
-  function createQuiz(elements: QuizProps) {
-    //
-    console.log(elements);
-  }
-
-  function addQuestion(question: string) {
-    console.log(question);
-  }
+  const [showModal, setShowModal] = useState(false);
 
   return (
-    <VStack>
-      <TeamName name={'Team name placeholder'} />
-      <Heading size={'xl'}>Create new quiz</Heading>
-      <Box>
-        <Text>Name of the quiz: </Text>
-        <Input placeholder="Basic usage" />
-      </Box>
-      <Box>
-        <Text>Add password: </Text>
-        <Input placeholder="Basic usage" />
-      </Box>
-      <Box>
-        <Text>Add PIN for users: </Text>
-        <Input placeholder="Basic usage" />
-      </Box>
-      <Box>
-        <Text>Number of rounds: </Text>
-        <Input placeholder="Basic usage" />
-      </Box>
-      <Box>
-        <Text>Topics: </Text>
-        <Input placeholder="Basic usage" />
-      </Box>
-      <Box>
-        <Text>Questions: </Text>
-        <Input placeholder="Basic usage" />
-      </Box>
-      <Button onClick={createQuiz()}>Start creating quiz!</Button>
-    </VStack>
+    <div>
+      <TeamName name={'Team name'} />
+      <NewGameModal isOpen={showModal} onClose={() => setShowModal(false)} />
+      <Button onClick={() => setShowModal(true)}>Create new quiz</Button>
+    </div>
   );
 }
