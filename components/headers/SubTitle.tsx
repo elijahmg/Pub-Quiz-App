fb3b9231b610f59b73e6ee48666d3e8570564960
Ilya <elijahmg25@gmail.com>
2023-01-14 17:16:44 +0100

    feat: fix ts

--- components/headers/SubTitle.tsx
+++ components/headers/SubTitle.tsx
@@ -1,10 +1,10 @@
-import { Text } from '@chakra-ui/react';
+import { TextProps, Text } from '@chakra-ui/react';
 import { ReactNode } from 'react';
 
 interface SubText {
   children: ReactNode;
 }
 
-export default function SubTitle({ children }: SubText) {
+export default function SubTitle({ children, ...props }: SubText & TextProps) {
   return <Text color="gray.400">{children}</Text>;
 }
