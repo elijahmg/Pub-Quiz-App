c6b7af73809e2f1b58bb49aabbc1193aa3103d57
Raul Calvo Martin <raul.calvo.martin@accenture.com>
2023-01-14 12:12:19 +0100

    add overview layout

--- components/Question.tsx
+++ components/Question.tsx
@@ -1,15 +1,24 @@
 import { Center, Stack, Text, Input, Button, Flex } from '@chakra-ui/react';
-import { useRef } from 'react';
+import { BaseSyntheticEvent, useCallback, useRef, useState } from 'react';
 import Constants from '../constants';
 
 export default function Question({
   question,
   handleAnswer,
+  answer = '',
 }: {
   question: string;
+  answer?: string;
   handleAnswer: (s: string) => void;
 }) {
   const input = useRef<HTMLInputElement | null>(null);
+  const [value, setValue] = useState(answer);
+  const inputHandler = useCallback(
+    (e: BaseSyntheticEvent<InputEvent>) => {
+      setValue(e.target.value);
+    },
+    [setValue],
+  );
   return (
     <Center>
       <Stack spacing={Constants.StackSpacing}>
@@ -18,7 +27,9 @@ export default function Question({
           <Input
             size="lg"
             ref={input}
+            value={value}
             placeholder="Do you think you know the answer?"
+            onInput={inputHandler}
           />
           <Button
             size="lg"
--- pages/[gameId]/play.tsx
+++ pages/[gameId]/play.tsx
@@ -1,22 +1,55 @@
-import { Progress, Center, Stack, Heading } from '@chakra-ui/react';
+import { Progress, Center, Stack } from '@chakra-ui/react';
+import { useCallback, useEffect, useState } from 'react';
 import Question from '../../components/Question';
 import TeamName from '../../components/TeamName';
 import Constants from '../../constants';
 
-function Play() {
+function Play({ channel }: { channel: any }) {
+  const [stateIndex, setStateIndex] = useState<number>(0);
+  const userName = 'John Doe';
+  const round = {
+    id: 1,
+    name: 'Music',
+    questions: [
+      {
+        id: 1,
+        content: 'Where is John',
+        answer: 'here',
+      },
+    ],
+  };
+  useEffect(() => {
+    channel.subscribe((status: any) => {
+      setStateIndex(Number(status));
+    });
+    return () => channel.unsubscribe();
+  }, [setStateIndex, channel]);
+
+  const userDidAnswer = useCallback(
+    (answer: string) => {
+      channel.send({
+        question: round.questions[stateIndex],
+        user: userName,
+        answer,
+      });
+    },
+    [channel, round.questions, stateIndex],
+  );
+
   return (
     <Center>
       <Stack spacing={Constants.StackSpacing}>
         <TeamName name="Dummy Team" />
 
         <Question
-          question="WTF?!"
-          handleAnswer={(answer: string) => {
-            // send answer to admin
-          }}
+          question={round.questions[stateIndex].content}
+          handleAnswer={userDidAnswer}
         />
 
-        <Progress size="lg" value={50} />
+        <Progress
+          size="lg"
+          value={Math.ceil((stateIndex + 1) / (round.questions.length ?? 1))}
+        />
       </Stack>
     </Center>
   );
