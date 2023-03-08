df4e587a07ce841557ec103e7ac5f32532c8dcad
Martin Mach <martin.mach@accenture.com>
2023-03-08 20:48:41 +0100

    feat: implement break route

--- pages/[gameId]/break.tsx
+++ pages/[gameId]/break.tsx
@@ -1,22 +1,28 @@
-import { Center, Image, Stack, Text } from '@chakra-ui/react';
+import { Center, Flex } from '@chakra-ui/react';
+import Header from '../../components/headers/Header';
+import SubHeader from '../../components/headers/SubHeader';
+import SubTitle from '../../components/headers/SubTitle';
+import { Alcoholism } from '../../components/images/alcoholism';
+import { MainPageWrapper } from '../../components/wrappers/main-page-wrapper';
 
 export default function Break() {
   return (
-    <Center>
-      <Stack>
-        <Text>Checking answers</Text>
-        <Image
-          maxW={300}
-          alt="Illustration placeholder for break"
-          src="https://cdn.vox-cdn.com/thumbor/-T7aAB2aRt3B_Aa3o6BU1qoIm6U=/0x0:5760x3840/1720x0/filters:focal(0x0:5760x3840):format(webp):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/23463325/2021_BreakEven_Photos_22.jpg"
-          fallbackSrc="https://cdn-icons-png.flaticon.com/512/3014/3014323.png"
-        />
-        <Text>Time for a break.</Text>
-        <Text>
-          Your quizmaster is checking your answers. In the meantime, have a chat
-          and/or drink, we do not judge.
-        </Text>
-      </Stack>
-    </Center>
+    <MainPageWrapper>
+      <Header textAlign="center" mb={10}>
+        Checking answers
+      </Header>
+      <Center flexDirection="column">
+        <Flex justifyContent="center" width="100%">
+          <Alcoholism />
+        </Flex>
+        <SubHeader mt={6} mb={2}>
+          Time for a break.
+        </SubHeader>
+        <SubTitle>
+          Your quizmasters is checking your answers. In the meantime, have a
+          chat and/or drink, we don’t judge :)
+        </SubTitle>
+      </Center>
+    </MainPageWrapper>
   );
 }
