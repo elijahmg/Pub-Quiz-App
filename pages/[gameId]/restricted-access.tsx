4cebdfc738e7997db9c2d1ad3ac0a48998a90cab
Martin Mach <martin.mach@accenture.com>
2023-03-09 12:05:42 +0100

    feat: restricted-access route implementation

--- pages/[gameId]/restricted-access.tsx
+++ pages/[gameId]/restricted-access.tsx
@@ -1,19 +1,23 @@
-import { Text, Stack, Image, Center } from '@chakra-ui/react';
+import { Center } from '@chakra-ui/react';
+import Header from '../../components/headers/Header';
+import SubHeader from '../../components/headers/SubHeader';
+import SubTitle from '../../components/headers/SubTitle';
+import { Denied } from '../../components/images/denied';
+import { MainPageWrapper } from '../../components/wrappers/main-page-wrapper';
 
 export default function RestrictedAccess() {
   return (
-    <Center>
-      <Stack>
-        <Text>Restricted Access</Text>
-        <Image
-          maxW={300}
-          alt="Illustration placeholder for restricted access"
-          src="https://i.kym-cdn.com/entries/icons/original/000/002/144/You_Shall_Not_Pass!_0-1_screenshot.jpg"
-          fallbackSrc="https://img.freepik.com/free-vector/flat-design-enter-sign_23-2149291772.jpg?w=1480&t=st=1673707379~exp=1673707979~hmac=f39dbe8597952709b1c658cf99f09c1ba6655aefbe6fd1bcaa967a9bc570a4b3"
-        />
-        <Text>Looks like the quiz has already started.</Text>
-        <Text>Sorry about that, but good luck next time!</Text>
-      </Stack>
-    </Center>
+    <MainPageWrapper>
+      <Header mb={14} alignSelf="center">
+        Restricted Access
+      </Header>
+      <Center flexDirection="column">
+        <Denied />
+        <SubHeader mt={14} mb={2}>
+          Looks like the quiz has already started.
+        </SubHeader>
+        <SubTitle>Sorry about that, but good luck next time!</SubTitle>
+      </Center>
+    </MainPageWrapper>
   );
 }
