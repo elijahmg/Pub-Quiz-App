ffd95bfb978f3cfc297de6454d262f7471e82f98
Martin Mach <martin.mach@accenture.com>
2023-05-15 14:34:37 +0200

    incorporate notes from revision

--- src/pages/[quizId]/break.tsx
+++ src/pages/[quizId]/break.tsx
@@ -1,21 +1,10 @@
 import { Center, Stack } from '@chakra-ui/react';
-import { useRouter } from 'next/router';
 import SubHeader from '../../components/headers/sub-header';
 import SubTitle from '../../components/headers/sub-title';
 import { Alcoholism } from '../../components/images/alcoholism';
 import { MainPageWrapper } from '../../components/wrappers/main-page-wrapper';
-import { useLocalWebsocketServer } from '../../local-services/useLocalWebsocketServer';
-import { QuizStatuses } from '../../server/types';
 
 const Break = () => {
-  const router = useRouter();
-
-  useLocalWebsocketServer((data) => {
-    if (data.status === QuizStatuses.SCORE_VIEWING) {
-      router.push({ pathname: 'overview', query: router.query });
-    }
-  });
-
   return (
     <Stack as={Center} spacing={6}>
       <Alcoholism />
