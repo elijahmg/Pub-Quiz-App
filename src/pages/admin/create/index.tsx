0b733974d1befbcb68454e89266d81e07e20a4df
Martin Mach <martin.mach@accenture.com>
2023-05-21 21:57:47 +0200

    feat: integrate formik lib, validate quiz creation first step

--- src/pages/admin/create/index.tsx
+++ src/pages/admin/create/index.tsx
@@ -1,4 +1,4 @@
-import { Flex, Heading } from '@chakra-ui/react';
+import { Flex, Heading, useToast } from '@chakra-ui/react';
 import { ReactElement } from 'react';
 import RouteNavigation from '../../../components/route-navigation';
 import SubHeader from '../../../components/headers/sub-header';
@@ -7,12 +7,36 @@ import { ADMIN_CREATE_ROUTE_LIST } from '../../../../constants';
 import { useAdminQuizManageContext } from '../../../components/contexts/admin-quiz-manage-context';
 import AdminQuizManageMainInfoForm from '../../../components/admin-quiz-manage/main-info-form';
 import { StoreQuiz } from '../../../../types';
+import { useFormikContext } from 'formik';
 
 const QuizCreate = () => {
   const { quizData, setQuizData } = useAdminQuizManageContext();
 
+  const toast = useToast();
+
+  const { validateForm, handleSubmit } = useFormikContext();
+
   const handleQuizDataChange = (quizData: StoreQuiz) => {
     setQuizData(quizData);
+  };
+
+  const handleNavigateNext = async () => {
+    handleSubmit();
+    const formErrors = await validateForm();
+    const isFormValid = !Object.keys(formErrors).length;
+
+    if (!isFormValid) {
+      toast({
+        title: 'Error',
+        description: 'Some fields weren`t filled correctly.',
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+        position: 'bottom-right',
+      });
+    }
+
+    return isFormValid;
   };
 
   return (
@@ -25,7 +49,10 @@ const QuizCreate = () => {
         quizData={quizData}
         onQuizDataChange={handleQuizDataChange}
       />
-      <RouteNavigation routeList={ADMIN_CREATE_ROUTE_LIST} />
+      <RouteNavigation
+        routeList={ADMIN_CREATE_ROUTE_LIST}
+        onNext={handleNavigateNext}
+      />
     </Flex>
   );
 };
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -6,3 +6,15 @@ export function isCSR() {
 export function generateRandomId() {
   return Math.floor(Math.random() * Math.floor(Math.random() * Date.now()));
 }
+
+// Get multiple functions and return a single one. When that one is called, it calls all the other functions,
+// passing them all the arguments
+export function getCombinedFunction<T extends unknown[]>(
+  ...functions: ((...args: T) => void)[]
+) {
+  return (...args: T) => {
+    functions.forEach((fn) => {
+      fn(...args);
+    });
+  };
+}
