cdc8760abadf9a31f70a47ca018f80f119e59022
MilanPavelcak <milan.pavelcak@sinnerschrader.com>
2023-09-15 11:58:22 +0200

    tests(playwright): add testids

--- src/components/headers/header.tsx
+++ src/components/headers/header.tsx
@@ -4,13 +4,15 @@ import useIsAdmin from '../../hooks/use-is-admin';
 
 export interface Props extends HeadingProps {
   children: ReactNode;
+  testId: string;
 }
 
-export default function Header({ children, ...props }: Props) {
+export default function Header({ children, testId, ...props }: Props) {
   const isAdmin = useIsAdmin();
 
   return (
     <Heading
+      data-testid={testId}
       size="2xl"
       color="green.100"
       textAlign={isAdmin ? 'left' : 'center'}
